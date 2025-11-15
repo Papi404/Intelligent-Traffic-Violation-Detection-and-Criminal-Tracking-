eb84734cb7d038c3611da585e1b389ae2abe3b89
Papi404 <154910609+Papi404@users.noreply.github.com>
2025-11-15 20:04:44 +0530

    Delete env.d.ts

--- env.d.ts
+++
@@ -1,4 +0,0 @@
-// This file tells TypeScript to trust that the '@google/genai' module exists.
-// It's necessary because we are loading it from a CDN via an importmap,
-// which the TypeScript compiler doesn't understand by default.
-declare module '@google/genai';
